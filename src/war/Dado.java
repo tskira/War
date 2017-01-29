2c70c0b41526541eb63c5362d97889f8b5dc7f26
Usuario <Usuario@Usuario-PC>
2017-01-29 17:30:48 -0200

    i. enum dado

---
+++ src/war/Dado.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package war;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum Dado {
+    
+    AZUL(0,3), //dado para atk aereo
+    VERMELHO(1,6), //dado para atk terrestre
+    AMARELO(1,6), //dado para defesa
+    PRETO(7,7); //dado para roubar no jogo
+    
+    private final int ini; //range do dado (valor inicial)
+    private final int fin; //range do dado (valor final)
+
+    Dado(int ini, int fin) { 
+        this.ini = ini;
+        this.fin = fin;
+    }
+    
+    public int lancarDado(){ //metodo para lançar um dado
+        return ((int)(Math.random()*fin + ini));
+    }
+
+}
