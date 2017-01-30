857fc7562bb0216c5cfa5d46378d2e47b7d8bb26
Usuario <Usuario@Usuario-PC>
2017-01-30 02:04:45 -0200

    i. enum continentes(3)/bugado

--- src/war/Continente.java
+++ src/war/Continente.java
@@ -16,34 +16,16 @@ import javafx.util.Pair;
  */
 public enum Continente {
     
-    AFRICA("Africa", Pair.with("Nigeria", int num[] = {3,4})),
-    AMERICA_NORTE("America do Norte",
-            Arrays.asList("Alasca", "Vancouver", "California", "Groenlandia",
-                          "Otawa", "Mexico", "Nova York")
-    ),
-    AMERICA_SUL("America do Sul",
-            Arrays.asList("Chile", "Colombia", "Argentina", "Brasil")
-    ),
-    ASIA("Asia",
-            Arrays.asList("Vladivostok", "Omsk", "Siberia", "Oriente Medio",
-                          "India", "China", "Japao")
-    ),
-    EUROPA("Europa",
-            Arrays.asList("Inglaterra", "Italia", "Suecia", "Alemanha",
-                          "Moscou")
-    ),
-    OCEANIA("Oceania",
-            Arrays.asList("Sumatra", "Borneu", "Australia", "Nova Guine")
-    );
+    AFRICA("Africa");
     
     private final String nome; //nome do continente
     private final Map<int[], Territorio> paises = new HashMap<>();
     
-    Continente(String nome, Pair<int[], String>){
+    Continente(String nome){
         this.nome = nome;
     }
     
-    public List<Territorio> getPaises(){
+    public Map<int[], String> getPaises(){
         return paises;
     }
 }
